9cd83d547f43d1a85ba6281ef01bbf924b115d32
aniketpatange <aniketpatange@users.noreply.github.com>
2020-05-28 23:52:23 +0530

    Code file created.

---
+++ Doc2/code.py
@@ -0,0 +1,66 @@
+# --------------
+#Importing header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+data=pd.read_csv(path)
+loan_status=data['Loan_Status'].value_counts()
+loan_status.plot(kind='bar')
+
+#Code starts here
+
+
+# --------------
+#Code starts here
+import matplotlib.pyplot as plt
+import pandas as pd
+property_and_loan=data.groupby(['Property_Area','Loan_Status']).size().unstack()
+property_and_loan.plot(kind='bar',stacked=False)
+plt.xlabel('Property Area')
+plt.ylabel('Loan Status')
+plt.xticks(rotation=45)
+
+
+# --------------
+#Code starts here
+education_and_loan=data.groupby(['Education','Loan_Status']).size().unstack()
+education_and_loan.plot(kind='bar',stacked=True)
+plt.xlabel('Education')
+plt.ylabel('Loan Status')
+plt.xticks(rotation=45)
+
+
+# --------------
+#Code starts here
+graduate=data[data['Education']=='Graduate']
+not_graduate=data[data['Education']=='Not Graduate']
+graduate.plot(kind='density',label='Graduate')
+not_graduate.plot(kind='density',label='Not Graduate')
+
+
+
+
+
+
+
+
+
+#Code ends here
+
+#For automatic legend display
+plt.legend()
+
+
+# --------------
+#Code starts here
+import matplotlib.pyplot as plt
+fig, (ax_1,ax_2,ax_3)=plt.subplots(3,1)
+data.plot.scatter(x='ApplicantIncome',y='LoanAmount',ax=ax_1)
+plt.title('Applicant Income')
+data.plot.scatter(x='CoapplicantIncome',y='LoanAmount',ax=ax_2)
+plt.title('Coapplicant Income')
+data['TotalIncome']=data['ApplicantIncome']+data['CoapplicantIncome']
+data.plot.scatter(x='TotalIncome',y='LoanAmount',ax=ax_3)
+plt.title('Total Income')
+
+
